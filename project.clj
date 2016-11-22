685cbe4464382ced58c518a7d8a65c99d3db6a35
Greg Leppert <greg@leppert.me>
2016-11-22 08:36:34 -0500

    Switch to Apache v2 license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject pal/pal-core "0.1.0-SNAPSHOT2"
   :description "A ClojureScript port of buddy-core"
   :url "https://github.com/leppert/pal-core"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
                  [com.cemerick/piggieback "0.2.1"]]
